72e75caaac4cdd28563975de8eb96185afd2ac08
David Chambers <dc@davidchambers.me>
2016-06-08 16:46:17 -0700

    lift and lift2

--- api.js
+++ api.js
@@ -9,9 +9,9 @@ const map = R.curry((f, functor) => functor.map(f));
 const ap = R.curry((applyF, applyX) => applyF.ap(applyX));
 
 // lift :: Functor f => (a -> b) -> f a -> f b
-const lift = R.curry((f, functor) => TK);
+const lift = map;
 
 // lift2 :: Apply f => (a -> b -> c) -> f a -> f b -> f c
-const lift2 = R.curry((f, applyA, applyB) => TK);
+const lift2 = R.curry((f, applyA, applyB) => ap(map(f, applyA), applyB));
 
 module.exports = {ap: ap, lift: lift, lift2: lift2, map: map};
